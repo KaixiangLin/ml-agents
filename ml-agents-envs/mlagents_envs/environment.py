ef41e66bdbff3cc0054cb7e1c756159e13b62ae5
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2020-11-17 15:59:06 -0500

    Export separate nodes for continuous/discrete actions (#4655)

--- ml-agents-envs/mlagents_envs/environment.py
+++ ml-agents-envs/mlagents_envs/environment.py
@@ -424,11 +424,13 @@ class UnityEnvironment(BaseEnv):
             if n_agents == 0:
                 continue
             for i in range(n_agents):
-                # TODO: This check will be removed when the oroto supports hybrid actions
-                if vector_action[b].continuous.shape[1] > 0:
-                    _act = vector_action[b].continuous[i]
-                else:
-                    _act = vector_action[b].discrete[i]
+                # TODO add separate fields for continuous and discrete actions in AgentActionProto
+                _act = []
+                if vector_action[b].continuous is not None:
+                    _act.append(vector_action[b].continuous[i])
+                if vector_action[b].discrete is not None:
+                    _act.append(vector_action[b].discrete[i])
+                _act = np.concatenate(_act, axis=0)
                 action = AgentActionProto(vector_actions=_act)
                 rl_in.agent_actions[b].value.extend([action])
                 rl_in.command = STEP
